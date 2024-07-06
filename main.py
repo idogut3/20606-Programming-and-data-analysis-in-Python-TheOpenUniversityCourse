af7c212501ef0f2b860f51b0fdea54adbea966c5
Ido Gutman <idogut3@gmail.com>
2024-07-06 23:54:55 +0300

    First function written load data- DOES NOT WORK YET YAY should fix it somehow (miss commited the first time now doing it again ~ido

---
+++ main.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+
+def load_data(file_name: str) -> np.ndarray:
+    if file_name != 'nasa.csv':
+        raise ValueError("File name must be nasa.csv")
+
+    try:
+        data = np.loadtxt(file_name)
+        return data
+    except FileNotFoundError:
+        raise FileNotFoundError("Cant find nasa.csv in your project, check your project resources")
+    except FileExistsError:
+        raise FileExistsError(
+            "Error with file, cannot read it (might be useful to check if the number of rows are equal to number of "
+            "columns)")
+    except Exception as e:
+        raise Exception(f"file error, an exception occurred: {e}")
+
+
+if __name__ == "__main__":
+    array = load_data('nasa.csv')
+    print(array)
+
